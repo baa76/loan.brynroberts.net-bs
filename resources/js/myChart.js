b77ea9bbe2e18bc567fdca1b4e7535da43a47754
baa76 <dev@brynroberts.net>
2018-03-15 07:53:56 +1100

    Update chart

--- resources/js/myChart.js
+++ resources/js/myChart.js
@@ -4,16 +4,24 @@ let chart = new Chart(ctx, {
     data: {
         labels: projectedPaymentDate,
         datasets: [{
+            label: "Current Balance",
+            backgroundColor: "rgba(80, 255, 123, 0.5)",
+            borderColor: "rgba(80, 255, 123, 0.5)",
+            lineTension: 0,
+            pointRadius: 1,
+            pointHoverRadius: 8,
+            cubicInterpolationMode: "monotone",
+            data: currentBalance,
+        }, {
             label: "Projected Balance",
             backgroundColor: "rgb(10,50,100)",
             borderColor: "rgb(10,50,100)",
             fill: false,
+            lineTension: 0,
+            pointRadius: 1,
+            pointHoverRadius: 8,
+            cubicInterpolationMode: "monotone",
             data: projectedCurrentBalance,
-        }, {
-            label: "Current Balance",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: currentBalance,
         }]
     },
     options: {
@@ -21,14 +29,19 @@ let chart = new Chart(ctx, {
             xAxes: [{
                 id: "x-axis-0",
                 type: "time",
+                distribution: "linear",
                 time: {
-                    unit: "quarter",
+                    unit: "month",
                     displayFormats: {
-                        quarter: "MMMM"
-                    }
+                        month: "MMM"
+                    },
+                    tooltipFormat: "ll"
                 },
                 ticks: {
-                    autoSkip: true
+                    autoSkip: true,
+                    maxRotation: 0,
+                    minRotation: 0,
+                    beginAtZero: true
                 }
             },
             {
@@ -36,25 +49,30 @@ let chart = new Chart(ctx, {
                 type: "time",
                 time: {
                     unit: "year",
-                    displayFormats: {
-                        year: "YYYY"
-                    }
+                    max: moment("31-12-2022", "DD-MM-YYYY")
                 },
                 gridLines: {
                     display: false
                 },
                 ticks: {
-                    autoSkip: true
+                    beginAtZero: true
                 }
             }],
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    min: "0",
+                    max: "5475",
+                    callback: function(value, index, values) {
+                        return "$ " + value;
+                    }
                 }
             }]
         },
     }
 });
 
+// chart.defaults.global.elements.point.radius = 1;
 
+console.log(chart.options.scales);
 console.log(chart);
